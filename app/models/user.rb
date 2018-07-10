3f78d91c8682f1b5b82af8ed752c09c6eb84be85
Nguyễn Thành Nhân <nhannt.bk@gmail.com>
2018-07-10 09:33:51 +0700

    Chapter 8 basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,16 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {minimum: Settings.user_setting.password_minimum_length}
 
+  # Returns the hash digest of the given string.
+  def self.digest _string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
+
   private
 
   def downcase_email
