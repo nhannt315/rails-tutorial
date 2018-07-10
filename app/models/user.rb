f9f8396432fa0c7fc90ada61e7af905cff99efc1
Nguyễn Thành Nhân <nhannt.bk@gmail.com>
2018-07-10 09:33:51 +0700

    Chapter 9 advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save :downcase_email
   has_secure_password
@@ -11,7 +12,6 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {minimum: Settings.user_setting.password_minimum_length}
 
-  # Returns the hash digest of the given string.
   def self.digest _string
     cost = if ActiveModel::SecurePassword.min_cost
              BCrypt::Engine::MIN_COST
@@ -19,6 +19,24 @@ class User < ApplicationRecord
              BCrypt::Engine.cost
            end
     BCrypt::Password.create string, cost: cost
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
   end
 
   private
