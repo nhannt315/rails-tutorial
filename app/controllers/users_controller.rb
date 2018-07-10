e25e67b03927ce756becb1df4846a0fea0bfd019
Nguyễn Thành Nhân <nhannt.bk@gmail.com>
2018-07-10 09:33:51 +0700

    Chapter 14 follow user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
-  before_action :get_user, only: [:index, :show, :update, :destroy]
+  before_action :get_user, only: [:index, :show, :update, :destroy, :following,
+    :followers]
 
   def index
     @users = User.get_activated.paginate page: params[:page]
@@ -43,6 +44,18 @@ class UsersController < ApplicationController
     @user.destroy
     flash[:success] = "User deleted"
     redirect_to users_url
+  end
+
+  def following
+    @title = t "users.following"
+    @users = @user.following.paginate page: params[:page]
+    render :show_follow
+  end
+
+  def followers
+    @title = t "users.follower"
+    @users = @user.followers.paginate page: params[:page]
+    render :show_follow
   end
 
   private
